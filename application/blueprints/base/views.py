873f7f26f5182f5ffcb718aa90beaa0c2c89e186
Colm Britton <colmjude@gmail.com>
2021-07-12 18:04:41 +0100

    Turn resource template into dynamic template

--- application/blueprints/base/views.py
+++ application/blueprints/base/views.py
@@ -1,10 +1,19 @@
+import json
 
 from flask import (
   render_template,
   Blueprint,
   current_app)
 
+
 base = Blueprint('base', __name__)
+
+def read_json_file(data_file_path):
+  f = open(data_file_path,)
+  data = json.load(f)
+  f.close()
+  return data
+
 
 @base.route('/')
 @base.route('/index')
@@ -19,4 +28,6 @@ def about(name):
 @base.route('/resource/')
 @base.route('/resource/<id>')
 def resource(id):
-  return render_template('resource.html', id=id)
+  # proposed interface
+  data = read_json_file("application/data/resource.json")
+  return render_template('resource.html', id=id, resource=data)
