1ae7ccadb67d91c320c0b4185d407570995a089f
Colm Britton <colmjude@gmail.com>
2021-07-13 11:32:25 +0100

    Add resource page example with no table

--- application/blueprints/base/views.py
+++ application/blueprints/base/views.py
@@ -8,6 +8,12 @@ from flask import (
 
 base = Blueprint('base', __name__)
 
+@base.context_processor
+def set_globals():
+  return {
+    "staticPath": "https://digital-land.github.io"
+  }
+
 def read_json_file(data_file_path):
   f = open(data_file_path,)
   data = json.load(f)
@@ -31,3 +37,10 @@ def resource(id):
   # proposed interface
   data = read_json_file("application/data/resource.json")
   return render_template('resource.html', id=id, resource=data)
+
+@base.route('/resource/no-table')
+@base.route('/resource/no-table/<id>')
+def resource_no_table(id):
+  # proposed interface
+  data = read_json_file("application/data/resource.json")
+  return render_template('resource-no-table.html', id=id, resource=data)
