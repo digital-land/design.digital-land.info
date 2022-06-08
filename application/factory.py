df67d2baa40ed300fe7dd648987100c17baa7d35
Paul Smith <mail@paulsmith.site>
2022-06-08 11:10:05 +0100

    adds new spec based feedback

--- application/factory.py
+++ application/factory.py
@@ -48,13 +48,14 @@ def register_context_processors(app):
 
 
 def register_filters(app):
-    from application.filters import hex_to_rgb_string, debug, get_items_beginning_with, from_json
+    from application.filters import hex_to_rgb_string, debug, get_items_beginning_with, from_json, import_csv
 
     app.add_template_filter(hex_to_rgb_string, name="hex_to_rgb")
     app.add_template_filter(debug, name="debug")
     app.add_template_filter(get_items_beginning_with, name="get_items_beginning_with")
     app.add_template_filter(unhyphenate, name="unhyphenate")
     app.add_template_filter(from_json, name="from_json")
+    app.add_template_filter(import_csv, name="import_csv")
 
 
 def register_extensions(app):
--- application/filters.py
+++ application/filters.py
@@ -1,5 +1,9 @@
 import json
 import jsonpickle
+import csv
+import os
+
+basePath = os.path.abspath(os.path.dirname(__file__))
 
 def hex_to_rgb_string(hex):
     h = hex.lstrip("#")
@@ -16,5 +20,15 @@ def unhyphenate(myString):
 def get_items_beginning_with(mylist, letter):
   return [item for item in mylist if item[0][0] == letter]
 
+
 def from_json(data):
     return json.loads(data)
+
+
+def import_csv(file):
+    # https://stackoverflow.com/questions/29432912/convert-a-csv-dictreader-object-to-a-list-of-dictionaries
+    filename = f"{basePath}/data/{file}"
+    new_dict = ""
+    with open(filename) as data:
+        new_dict = list(csv.DictReader(data))
+    return new_dict
